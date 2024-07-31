1da1bf8eb0b968cb8aec8e454c2a4342e2d1df79
Th3C0d3Mast3r <acharyadevesh1105@gmail.com>
2024-07-31 21:42:18 +0530

    git add

---
+++ MathFunctions.py
@@ -0,0 +1,8 @@
+# This is a file that deals with some of the math functions
+# for this to work, we need to import math module
+
+import math                     # and yes, thats the complete syntax 
+pi=3.14                         # well, actually what I did is:- float pi=3.14;
+
+print(round(pi))
+print(math.ceil(3245.23))
