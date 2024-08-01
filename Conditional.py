21592a13063f567f56f83230daa5e7f74afe8dbe
Th3C0d3Mast3r <acharyadevesh1105@gmail.com>
2024-08-01 19:53:12 +0530

    git upload

--- Conditional.py
+++ Conditional.py
@@ -11,4 +11,17 @@ elif(age>105):
 else:
     print("KIDS")
 
-# a random if-elif-else block
+# a random if-elif-else block (these are th conditional statements that you can understand and easily use)
+
+# We know head on to see conditional OPRTATORS
+
+temp=float(input("What is the TEMPERATURE around: "))
+
+if(temp>=0 and temp<=30):                       # the && of Java is 'and' in Python
+    print("IT IS A MODERATE TEMPERATURE")
+elif(temp<0 or temp>30):                        # the || of java is 'or' in Python
+    print("ITS A BAD WEATHER OUTSIDE!")
+elif(not(temp>0 and temp<50)):                  # the ! of Java is 'not' in Python
+    print("HELICOPTER-HELICOPTER")
+else:
+    print("SIKE")
