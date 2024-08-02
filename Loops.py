845a9e5fa8f042957af66b5d24f49188e39d28ba
Th3C0d3Mast3r <acharyadevesh1105@gmail.com>
2024-08-02 19:15:56 +0530

    git upload

--- Loops.py
+++ Loops.py
@@ -3,6 +3,9 @@
 # while loops
 # do-while loops
 
+
+import time
+
 name=""
 while len(name)==0:
     name=input("Enter Your Name: ")             # this will be looped until someone types the name
@@ -19,3 +22,39 @@ for j in range(50, 100):                        # this is like:-  for(int j=50; 
 print("NEW LOOP FROM 10 TO 39 WITH JUMPS OF 3")
 for k in range(10, 40, 3):
     print(k)
+
+# we can even loop for strings
+print("Looping through the new name")
+newName="Hello this is a Looping in String"
+for i1 in newName:
+    print(i1)                                   # the printing in loops is pre println form
+
+print("\n\n\n\n\n")
+# we import the time module here for this execution
+for seconds in range(10, 0, -1):
+    print(seconds)
+    time.sleep(1)                               # this is same is thread sleep and all that we knew in Java
+print("HAPPY NEW YEAR")
+
+rows=int(input("Enter the Number of Rows: "))
+columns=int(input("Enter the Number of Columns: "))
+
+for i in range(rows):                           # example of nested loops
+    for j in range(columns):
+        print("$", end="")                      # 
+    print()                                     # this is for that orientation and all
+
+'''
+NOW WE LOOK AT THE LOOP CONTROL STATEMENTS:
+They are mainly 3:-
+- break
+- continue
+- pass
+These are the basic ones and I won't be explaining them as their use remains the same, as in Java
+'''
+
+for i in range(1, 21):                          # pass is like, let go of it-but continue with the next one
+    if(i==13):
+        pass
+    else:
+        print(i)
