2d0b3a273f6decad33b5bd2a50e93f4f91168c30
Th3C0d3Mast3r <acharyadevesh1105@gmail.com>
2024-08-03 15:35:18 +0530

    git upload

--- Functions.py
+++ Functions.py
@@ -14,3 +14,21 @@ public boolean isTrueFunction(String response)
 }
 ''' 
 
+def functions():                                            # def is the thing that makes it function and () too
+    print("INSIDE THE FUNCTION")                            # THE INDENTED THING MAKES IT INSIDE THE FUNCTION BLOCK
+
+functions()                                                 # CALLING THE FUNCTION
+
+
+name=input("ENTER YOUR NAME:- ")
+# secondFn(name)                                            THIS WONT WORK BECAUSE THIS FEATURE IS LIKE C LANGUAGE
+
+def secondFn(parameter1):
+    print("This is the parameter passed:- "+parameter1)
+
+secondFn(name)
+
+integ=int(input("ENTER THE INTEGER:- "))
+def thirdFn(integerVal):
+    print("THIS IS THE INTEGER VALUE PASSED:- ", integerVal)
+thirdFn(integ)
