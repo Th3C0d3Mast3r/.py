97d47a64b7df69ad2f0f13256a4af94645556e8c
Th3C0d3Mast3r <acharyadevesh1105@gmail.com>
2024-08-03 15:56:17 +0530

    git upload

--- Functions.py
+++ Functions.py
@@ -32,3 +32,50 @@ integ=int(input("ENTER THE INTEGER:- "))
 def thirdFn(integerVal):
     print("THIS IS THE INTEGER VALUE PASSED:- ", integerVal)
 thirdFn(integ)
+
+'''RETURN STATEMENT IN THE FUNCTIONS'''
+# IN JAVA, THE void or boolean IS THE RETURN TYPE
+# WHICH MEANS, THE RETURN TYPE OF THAT FUNCTION WOULD BE OF nothing or boolean ACCORDINGLY
+
+x=int(input("ENTER THE FIRST INTEGER:- "))
+y=int(input("ENTER THE SECOND INTEGER:- "))
+
+def multi(int1, int2):
+    result=int1*int2                                            # WE NEED NOT MAKE A VARIABLE TO STORE IT-I JUST DID FOR SAKE OF UNDERSTANDING
+    return result
+
+results=multi(x,y)
+print("\n\nTHEIR MULTIPLICATION RESULT IS:- ",results)
+
+
+
+# some more of python functions and their property
+
+def greeting(firstN, middleN, lastN):
+    print("GREETINGS!\nYou must be "+firstN+" "+middleN+" "+lastN)
+
+greeting("firstName", "middleName", "lastName")                 # THEY ARE ORDERED PARAMETER PASSING
+print("\n")
+greeting(firstN="Suresh", lastN="Sharma", middleN="Mohit")      # THUS, THEY NEED NOT BE ORDERED, BUT ARE ASSIGNED PROPERLY
+
+
+# NESTED FUNCTION CALLS IN PYTHON
+
+'''
+nums=input("ENTER A NON-NEGATIVE INTEGRAL NUMBER:- ")
+nums=float(nums)
+nums=abs(nums)
+nums=round(nums)
+print("YOU ENTERED:- ", nums)
+'''
+# the above can be written as:-
+print("YOU ENTERED:- ",round(abs(float(input("ENTER A NON-NEGATIVE INTEGRAL NUMBER:- ")))))
+
+
+# SCOPE OF VARIABLE (this is in short, local, instance , global, static and all those variable types as in Java)
+# not writing much on this as it is something easy and no need for any code for that
+# ORDER OF PREFERENCE-   LEGB:- Local --> Enclosing --> Global --> Built-In
+
+# *args formatting in Python
+
+
