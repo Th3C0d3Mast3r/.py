bbe2a0696a67a9e9b197db8930a5982034b5f133
Th3C0d3Mast3r <acharyadevesh1105@gmail.com>
2024-08-07 23:09:20 +0530

    git upload

---
+++ Modules.py
@@ -0,0 +1,17 @@
+# MODULE:- A File containing python codes in it. May have functions or other classes(similar to java packages and all)
+# Helps break the code into several small bits and use the ones that are often needed
+
+import moduleSide as kol            # writing 'as kol' means it is equivalent to the object name
+
+# below I will be calling the functions from that other file
+kol.hello()
+print("\n")
+kol.hello2()
+
+'''
+THERE IS ANOTHER METHOD FOR IMPORTHING, THAT IS:-
+from moduleSide import *
+'''
+
+# to know more about IN-BUILT MODULES, type this once:-  help('modules')
+help('modules')
---
+++ moduleSide.py
@@ -0,0 +1,8 @@
+def hello():                                                # this is a function in the file
+    print("HELLO WORLD IS PRINTED")
+    print("ENTERING SOME NUMBER HERE {:^10}".format(10))
+
+def hello2():
+    print("WHAT IS WRONG WITH YOU")
+    print("GET A LIFE, GEEK")
+    
